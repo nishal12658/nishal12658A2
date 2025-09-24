c4e4171e14e5357d35cbfb70234761614fded1b1
nishal <nishal@gmail.com>
2025-09-25 01:17:16 +0500

    working on creating the first api for getting all the events

--- nishal12658A2-api/routes.js
+++ nishal12658A2-api/routes.js
@@ -6,6 +6,11 @@ router.get('/', (req, res) => {
   res.json({ message: 'API is working!' });
 });
 
+// Get all events
+router.get('/events', async (req, res) => {
+  const [rows] = await db.query('SELECT * FROM events');
+  res.json(rows);
+});
 
 
 module.exports = router;
