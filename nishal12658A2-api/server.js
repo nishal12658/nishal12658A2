4fc91c81e80043e7c687d34931fbd0a5a2c6f01d
nishal <nishal@gmail.com>
2025-09-26 02:16:48 +0500

    working on to integrate to the server side

--- nishal12658A2-api/server.js
+++ nishal12658A2-api/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path'); // <-- Add this line
 const app = express();
 const routes = require('./routes');
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../nishal12658A2-clientside')));
 app.use('/api', routes);
 
 const PORT = 3000;
